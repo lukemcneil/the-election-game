2ec67da02251c5abb8b6e26ca7d5976d6c571e80
David McNeil <mcneil.david2@gmail.com>
2019-12-29 21:16:19 -0500

    Add starter client
    Signed-off-by: David McNeil <mcneil.david2@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Weighty Inquiry
+
+A game of asking questions and guessing answers.
+
+## Design
+
+TODO
---
+++ client/src/Main.elm
@@ -0,0 +1,71 @@
+module Main exposing (main)
+
+-- Press buttons to increment and decrement a counter.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/buttons.html
+--
+
+import Browser
+import Html exposing (Html, button, div, h1, text)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+init : Model
+init =
+    0
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+
+-- VIEW
+
+
+control : Msg -> String -> Html Msg
+control msg content =
+    button [ onClick msg, class "bg-blue-600 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded" ] [ text content ]
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "text-center" ]
+        [ h1 [ class "text-4xl" ] [ text "Counter" ]
+        , control Decrement "-"
+        , div [ class "text-2xl" ] [ text (String.fromInt model) ]
+        , control Increment "+"
+        ]
