623c7ed073b86fb0a085195b249f829ea463691c
Luke McNeil <lukemcneil61@gmail.com>
2022-12-21 13:24:10 -0500

    change port to 1234

--- server-rust/src/main.rs
+++ server-rust/src/main.rs
@@ -162,7 +162,7 @@ struct Opt {
     #[structopt(long = "host", short = "H", default_value = "0.0.0.0")]
     host: String,
     /// A port number to listen on.
-    #[structopt(long = "port", short = "P", default_value = "8172")]
+    #[structopt(long = "port", short = "P", default_value = "1234")]
     port: u16,
     /// The log level.
     #[structopt(
