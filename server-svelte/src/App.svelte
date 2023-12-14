65bdae0d76b7b0d97af0d48a8904acbe00c8a457
Adam <adamwmcneil@gmail.com>
2023-12-14 17:59:19 -0600

    added init server-svelte folder

---
+++ server-svelte/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import InputField from './lib/InputField.svelte';
+  import Button from './lib/Button.svelte'
+  let joinGameText = "Join Game"
+  let name = "";
+  let room_name = "";
+  function onClick() {
+    console.log(name);
+  }
+
+  function onClickJoinRoom() {
+    console.log(room_name);
+  }
+</script>
+
+<main>
+  <h1>Weight Inquiries</h1>
+
+  <div class="card">
+    <InputField bind:value="{name}" text="enter your name" />
+  </div>
+
+  <div class="card">
+    <InputField bind:value="{room_name}" text="enter the game room"/>
+  </div>
+  
+  <div class="card">
+    <Button text="Join Game" onClick={onClick} />
+  </div>
+
+  <div class="card">
+    <Button text="Create Game" onClick={onClickJoinRoom}/>
+  </div>
+
+</main>
