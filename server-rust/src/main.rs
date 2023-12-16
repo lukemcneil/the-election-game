c4d0e526dcaeea297fb950b93f9328d6f9da48c4
Adam <adamwmcneil@gmail.com>
2023-12-16 15:48:56 -0600

    changed rust server to serve the svelte project

--- server-rust/src/main.rs
+++ server-rust/src/main.rs
@@ -135,7 +135,7 @@ fn rocket(opt: Option<Opt>) -> rocket::Rocket {
     rocket::ignite().attach(cors.to_cors().unwrap());
     rocket
         .attach(cors.to_cors().unwrap())
-        .mount("/", StaticFiles::from("../client-elm/"))
+        .mount("/", StaticFiles::from("../client-svelte/build/"))
         .mount(
             "/api/v1",
             routes![
